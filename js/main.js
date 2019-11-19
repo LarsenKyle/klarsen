a85bf5e0b53e19f8f20aacc15f18aec7dc5d22a1
LarsenKyle <krlars87@gmail.com>
2019-11-19 15:58:51 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -4,11 +4,56 @@ const menu = document.querySelector(".menu")
 const menuBranding = document.querySelector(".menu-branding")
 const menuNav = document.querySelector(".menu-nav")
 const navItems = document.querySelectorAll(".nav-item")
+const workBtn = document.querySelector(".view-btn")
 
+//Pages
+const contactPage = document.getElementById("contact")
+const aboutPage = document.getElementById("about")
+const workPage = document.getElementById("work")
+const homePage = document.getElementById("home")
+
+//Link Array, Page Array
+const pageLinks = document.querySelectorAll(".link")
+const pagesArray = [contactPage, aboutPage, workPage, homePage]
+
+//Listeners
 let showMenu = false
-window.addEventListener("scroll", activeMenu)
+// window.addEventListener("scroll", activeMenu)
 menuBtn.addEventListener("click", toggleMenu)
 navItems.forEach(item => item.addEventListener("click", toggleMenu))
+navItems.forEach(item => item.addEventListener("click", menuMove))
+pageLinks.forEach(item => item.addEventListener("click", menuMove))
+workBtn.addEventListener("click", menuMove)
+
+function menuMove(e) {
+  console.log(e.target)
+  pagesArray.forEach(page => {
+    if (page.classList.contains("it")) {
+      page.classList.remove("it")
+    }
+  })
+  pageLinks.forEach(link => {
+    if (link.firstChild.classList.contains("active")) {
+      link.firstChild.classList.remove("active")
+    }
+  })
+  if (e.target.classList.contains("home")) {
+    homePage.classList.add("it")
+    pageLinks[0].firstChild.classList.add("active")
+  }
+  if (e.target.classList.contains("about")) {
+    aboutPage.classList.add("it")
+    pageLinks[1].firstChild.classList.add("active")
+  }
+  if (e.target.classList.contains("work")) {
+    workPage.classList.add("it")
+    pageLinks[2].firstChild.classList.add("active")
+  }
+  if (e.target.classList.contains("con")) {
+    contactPage.classList.add("it")
+    pageLinks[3].firstChild.classList.add("active")
+  }
+}
 
 function toggleMenu() {
   if (!showMenu) {
@@ -27,66 +72,52 @@ function toggleMenu() {
     showMenu = false
   }
 }
-function activeMenu() {
-  const home = document.getElementById("home")
-  const about = document.getElementById("about")
-  const work = document.getElementById("work")
-  const homeLink = document.querySelector(".home")
-  const aboutLink = document.querySelector(".about")
-  const workLink = document.querySelector(".work")
-  const contactLink = document.querySelector(".con")
-  const links = document.querySelectorAll("a")
+// function activeMenu() {
+//   const home = document.getElementById("home")
+//   const about = document.getElementById("about")
+//   const work = document.getElementById("work")
+//   const homeLink = document.querySelector(".home")
+//   const aboutLink = document.querySelector(".about")
+//   const workLink = document.querySelector(".work")
+//   const contactLink = document.querySelector(".con")
+//   const links = document.querySelectorAll("a")
 
-  if (window.scrollY < 400) {
-    links.forEach(link => {
-      if (link.classList.contains("active")) {
-        link.classList.remove("active")
-        homeLink.classList.add("active")
-      }
-    })
-  }
-  if (
-    window.scrollY > home.offsetTop + home.offsetHeight &&
-    window.scrollY < about.offsetTop + about.offsetHeight
-  ) {
-    links.forEach(link => {
-      if (link.classList.contains("active")) {
-        link.classList.remove("active")
-        aboutLink.classList.add("active")
-      }
-    })
-  }
-  if (
-    window.scrollY > about.offsetTop + about.offsetHeight &&
-    window.scrollY < work.offsetTop + work.offsetHeight
-  ) {
-    links.forEach(link => {
-      if (link.classList.contains("active")) {
-        link.classList.remove("active")
-        workLink.classList.add("active")
-      }
-    })
-  }
-  if (window.scrollY > work.offsetTop + work.offsetHeight) {
-    links.forEach(link => {
-      if (link.classList.contains("active")) {
-        link.classList.remove("active")
-        contactLink.classList.add("active")
-      }
-    })
-  }
-}
-// function changeFont() {
-//   const fontInput = document.getElementById("font").value
-//   const googleFont = document.getElementById("googleFont").value
-//   const head = document.head
-//   const body = document.body
-//   let link = document.createElement("link")
-
-//   link.type = "text/css"
-//   link.rel = "stylesheet"
-//   link.href = googleFont
-//   head.appendChild(link)
-
-//   body.style.fontFamily = fontInput
+//   if (window.scrollY < 400) {
+//     links.forEach(link => {
+//       if (link.classList.contains("active")) {
+//         link.classList.remove("active")
+//         homeLink.classList.add("active")
+//       }
+//     })
+//   }
+//   if (
+//     window.scrollY > home.offsetTop + home.offsetHeight &&
+//     window.scrollY < about.offsetTop + about.offsetHeight
+//   ) {
+//     links.forEach(link => {
+//       if (link.classList.contains("active")) {
+//         link.classList.remove("active")
+//         aboutLink.classList.add("active")
+//       }
+//     })
+//   }
+//   if (
+//     window.scrollY > about.offsetTop + about.offsetHeight &&
+//     window.scrollY < work.offsetTop + work.offsetHeight
+//   ) {
+//     links.forEach(link => {
+//       if (link.classList.contains("active")) {
+//         link.classList.remove("active")
+//         workLink.classList.add("active")
+//       }
+//     })
+//   }
+//   if (window.scrollY > work.offsetTop + work.offsetHeight) {
+//     links.forEach(link => {
+//       if (link.classList.contains("active")) {
+//         link.classList.remove("active")
+//         contactLink.classList.add("active")
+//       }
+//     })
+//   }
 // }
